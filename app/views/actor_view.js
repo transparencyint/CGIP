d497401f30e982428a04acfd635e50603f7cee7b
Felix Niklas <mrflix@gmail.com>
2012-04-25 00:34:27 +0200

    add drag-and-drop support

--- app/views/actor_view.js
+++ app/views/actor_view.js
@@ -6,8 +6,33 @@ module.exports = View.extend({
   
   className : 'actor',
   
+  events : {
+    mousedown : "startToDrag",
+  },
+  
   initialize: function(){
     
+  },
+  
+  startToDrag : function(event){
+    event.preventDefault();
+    
+    if(event.button === 2) return true; // right-click
+    
+    var myOffset = this.$el.offset();
+    startPos = { 
+      x : normalizedX(event) - myOffset.left,
+      y : normalizedY(event) - myOffset.top
+    };
+    $(document).bind(inputMove, $.proxy( this, "moveElement"));
+    this.moveElement(event);
+  },
+
+  moveElement : function(event){
+      this.$el.css({ 
+        top : normalizedY(event) - startPos.y,
+        left : normalizedX(event) - startPos.x
+      });
   },
   
   getRenderData : function(){
@@ -22,3 +47,26 @@ module.exports = View.extend({
     });
   },
 });
+
+var inputDown, inputMove, inputUp;
+
+if (window.Touch) {
+	inputDown = "touchstart";
+	inputMove = "touchmove";
+	inputUp = "touchend";
+}
+else {
+	inputDown = "mousedown";
+	inputMove = "mousemove";
+	inputUp = "mouseup";
+}
+
+$(document).bind(inputUp, function(){ $(this).unbind(inputMove); });
+
+function normalizedX(event){
+	return window.Touch ? event.originalEvent.touches[0].pageX : event.pageX;
+}	
+
+function normalizedY(event){
+	return window.Touch ? event.originalEvent.touches[0].pageY : event.pageY;
+}
