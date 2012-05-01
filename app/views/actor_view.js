5870cd3c8d8f8b85dca55fb165dabc0a90af0ec9
Felix Niklas <mrflix@gmail.com>
2012-05-02 01:12:50 +0200

    add drag-n-drop sync animation

--- app/views/actor_view.js
+++ app/views/actor_view.js
@@ -28,14 +28,26 @@ module.exports = View.extend({
       y : normalizedY(event) - myOffset.top - this.$el.height()/2
     };
     $(document).bind(inputMove, $.proxy( this, "moveElement"));
+    $(document).one(inputUp, $.proxy( this, "stopMoving"));
     this.moveElement(event);
   },
 
   moveElement : function(event){
-      this.$el.css({ 
-        top : normalizedY(event) - startPos.y,
-        left : normalizedX(event) - startPos.x
-      });
+    this.$el.css({ 
+      top : normalizedY(event) - startPos.y,
+      left : normalizedX(event) - startPos.x,
+      'WebkitTransition' : ''
+    });
+  },
+  
+  stopMoving : function(){
+    $(document).unbind(inputMove);
+    this.model.save({ 
+      'pos' : {
+        x : this.$el.offset().left + this.$el.width()/2,
+        y : this.$el.offset().top + this.$el.height()/2
+      }
+    });
   },
   
   getRenderData : function(){
@@ -49,6 +61,7 @@ module.exports = View.extend({
     this.$el.css({
       left : pos.x,
       top : pos.y,
+      WebkitTransition : '500ms'
     });
 
     this.$el.find('.name').text(name);
@@ -67,8 +80,6 @@ else {
 	inputMove = "mousemove";
 	inputUp = "mouseup";
 }
-
-$(document).bind(inputUp, function(){ $(this).unbind(inputMove); });
 
 function normalizedX(event){
 	return window.Touch ? event.originalEvent.touches[0].pageX : event.pageX;
