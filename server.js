54441588bbe2a540d34587422adc9c78ee6e269e
Jan Monschke <jan.monschke@gmail.com>
2012-05-01 19:58:01 +0200

    make server work with our real db, fuck yeah!

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ var app = express.createServer();
 
 // The remote database information
 var PREFIX = '/db/';
-var TARGET = 'http://localhost';
-var PORT = 5984;
+var TARGET = 'http://cgip.iriscouch.com';
+var PORT = 80;
 
 // This app's port
 var appPort = process.env['app_port'] || 3000;
@@ -20,7 +20,7 @@ function couchDBForward(req, res, next){
   if(u.pathname.substring(0, PREFIX.length) != PREFIX)
     next(); // this may be a static page request or whatever
   else{
-    u = TARGET + ':' + PORT + u.pathname.substring(PREFIX.length-1) + (u.search||'');
+    u = TARGET + u.pathname.substring(PREFIX.length-1) + (u.search||'');
     couchDBRequest(req, res, u);
   }
 };
@@ -40,35 +40,36 @@ function unknownError(response, e) {
 function couchDBRequest(inRequest, inResponse, uri) {
     util.log(inRequest.method + ' ' + uri);
     uri = url.parse(uri);
-    var out = http.createClient(uri.port, uri.hostname);
+    var outPort = (uri.port || 80);
     var path = uri.pathname + (uri.search || '');
     var headers = inRequest.headers;
-    headers['host'] = uri.hostname + ':' + uri.port;
+    headers['host'] = uri.hostname + ':' + outPort;
     headers['x-forwarded-for'] = inRequest.connection.remoteAddress;
-    headers['referer'] = 'http://' + uri.hostname + ':' + uri.port + '/';
+    headers['referer'] = 'http://' + uri.hostname + ':' + outPort + '/';
 
-    var outRequest = out.request(inRequest.method, path, headers);
-
-    out.on('error', function(e) {
-      unknownError(inResponse, e);
-    });
-    outRequest.on('error', function(e) {
-      unknownError(inResponse, e)
-    });
-
-    inRequest.on('data', function(chunk) {
-      outRequest.write(chunk);
-    });
-    inRequest.on('end', function() {
-      outRequest.on('response', function(outResponse) {
-        if (outResponse.statusCode == 503) {
-          return error(inResponse, 'db_unavailable', 'Database server not available.', 502);
-        }
-        inResponse.writeHead(outResponse.statusCode, outResponse.headers);
-        outResponse.on('data', function(chunk) { inResponse.write(chunk); });
-        outResponse.on('end', function() { inResponse.end(); });
+    var outRequest = http.request({
+      hostname: uri.hostname,
+      path: path,
+      port: outPort,
+      method: inRequest.method,
+      headers: headers
+    },function(res){
+      res.on('data', function(chunk){
+        inResponse.write(chunk);
       });
-      outRequest.end();
+
+      res.on('end', function(){
+        inResponse.end();
+      });
+    });
+
+    //var outRequest = out.request(inRequest.method, path, headers);
+    if(inRequest.method == 'POST')
+      outRequest.write(JSON.stringify(inRequest.body));
+
+    outRequest.on('error', function(e) {
+      console.log('outRequest#error');
+      unknownError(inResponse, e);
     });
 };
 
