6bb9c351505802f324123beddc517ad6e4a4690a
Tommy Krueger <info@tommykrueger.com>
2012-05-08 17:51:54 +0200

    added table views

---
+++ app/views/import_table_row_view.js
@@ -0,0 +1,45 @@
+var View = require('./view');
+var Import = require('models/import');
+
+module.exports = View.extend({
+  
+  template: require('./templates/import_table_row'),
+  
+  className : '',
+  tagName : 'tr',
+  
+  initialize: function(){
+
+  },
+  
+  getRenderData : function(){
+    
+  },
+  
+  afterRender: function(){
+   console.log(this.model);
+  },
+});
+
+var inputDown, inputMove, inputUp;
+
+if (window.Touch) {
+  inputDown = "touchstart";
+  inputMove = "touchmove";
+  inputUp = "touchend";
+}
+else {
+  inputDown = "mousedown";
+  inputMove = "mousemove";
+  inputUp = "mouseup";
+}
+
+$(document).bind(inputUp, function(){ $(this).unbind(inputMove); });
+
+function normalizedX(event){
+  return window.Touch ? event.originalEvent.touches[0].pageX : event.pageX;
+} 
+
+function normalizedY(event){
+  return window.Touch ? event.originalEvent.touches[0].pageY : event.pageY;
+}
