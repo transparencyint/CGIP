d4b68dec73ce5565b2b064c8f6017d77d4f3b854
Tommy Krueger <info@tommykrueger.com>
2012-05-08 14:43:58 +0200

    added import route

--- app/lib/router.js
+++ app/lib/router.js
@@ -1,14 +1,28 @@
 var application = require('application');
 var ActorEditor = require('views/actor_editor');
+var ImportView = require('views/import_view');
 
 module.exports = Backbone.Router.extend({
   routes: {
-    '': 'home'
+    '': 'home',
+    'edit/:country/money/import': 'import'
   },
 
   home: function() {
     var editor = new ActorEditor();
     editor.render();
     $('body').append( editor.el );
+  },
+
+  import: function(country) {
+  	switch(country)
+  	{
+  		case 'bd':
+  			var editor = new ImportView();
+    		editor.render();
+    		$('body').append( editor.el );
+  		break;
+  	}
+    
   }
 });
