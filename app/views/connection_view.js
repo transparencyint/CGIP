01bd1a0299f9462899b847b3497bde63ee07c532
Felix Niklas <mrflix@gmail.com>
2012-05-15 16:27:48 +0200

    optimize drag performance

--- app/views/connection_view.js
+++ app/views/connection_view.js
@@ -13,11 +13,10 @@ module.exports = View.extend({
     }
 
     if(this.from)
-      this.from.on('change', this.render, this);
+      this.from.on('change:pos', this.update, this);
       
-
     if(this.to)
-      this.to.on('change', this.render, this);
+      this.to.on('change:pos', this.update, this);
   },
 
   getRenderData : function(){
@@ -25,26 +24,25 @@ module.exports = View.extend({
   },
   
   afterRender: function(){
-    var posFrom = this.from.get('pos');    
-    var posTo = this.to.get('pos');
-
-    this.update(posFrom.x, posFrom.y, posTo.x, posTo.y, '3', 'white');
+    this.ctx = this.$el.get(0).getContext('2d');
+    this.update();
   },
 
-  update: function(x0, y0, x1, y1, size, color){
-    this.size = size || this.size;
-    this.color = color || this.color;
+  update: function(){
+    var from = this.from.get('pos');    
+    var to = this.to.get('pos');
+    
     this.pos = {
-      x : Math.min(x0, x1),
-      y : Math.min(y0, y1)
+      x : Math.min(from.x, to.x),
+      y : Math.min(from.y, to.y)
     }
     this.start = {
-      x : x0 - this.pos.x,
-      y : y0 - this.pos.y,
+      x : from.x - this.pos.x,
+      y : from.y - this.pos.y,
     };
     this.end = {
-      x : x1 - this.pos.x,
-      y : y1 - this.pos.y,
+      x : to.x - this.pos.x,
+      y : to.y - this.pos.y,
     };
     
     this.width = Math.abs(this.end.x - this.start.x);
@@ -58,24 +56,17 @@ module.exports = View.extend({
       x : this.end.x,
       y : this.start.y,
     };
-    
-    this.renderCanvas();
-  },
-  
-  renderCanvas: function(){
-    this.ctx = this.$el.get(0).getContext('2d');
-    
+      
     this.$el.attr({
       'width': this.width,
       'height': this.height
-    });
-    this.$el.css({
+    }).css({
       'left': this.pos.x + "px",
       'top': this.pos.y + "px"
     });
     
-    this.ctx.lineWidth = this.size;
-    this.ctx.strokeStyle = this.color;
+    this.ctx.lineWidth = 2;
+    this.ctx.strokeStyle = 'white';
     this.ctx.lineCap = 'round';
     this.ctx.lineJoin = 'round';
     
@@ -83,7 +74,6 @@ module.exports = View.extend({
     this.ctx.moveTo(this.start.x, this.start.y);
     this.ctx.bezierCurveTo(this.cp1.x, this.cp1.y, this.cp2.x, this.cp2.y, this.end.x, this.end.y);
     this.ctx.stroke();
-
   }
 
 });
