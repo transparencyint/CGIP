192057efe7ca1f73187366abf2dc13c069c56290
Jan Monschke <jan.monschke@gmail.com>
2012-05-16 15:59:38 +0200

    add deployment files

--- app/initialize.js
+++ app/initialize.js
@@ -12,5 +12,6 @@ $(function() {
 
 // overwrite the defaul requests, because /db is the new db namespace
 $.ajaxPrefilter(function( options, originalOptions, jqXHR ) {
-  options.url = '/db' + options.url;
+  if(window.location.href.indexOf('localhost:3000') != -1 || window.location.href.indexOf('127.0.0.1:3000') != -1)
+    options.url = '/db' + options.url;
 });
