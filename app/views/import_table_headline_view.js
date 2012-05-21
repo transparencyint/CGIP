41f7008e01baaaafd2c376d931b4f5a48194d0c6
Tommy Krueger <info@tommykrueger.com>
2012-05-21 16:14:34 +0200

    reworked test.csv with apostrophs

--- app/views/import_table_headline_view.js
+++ app/views/import_table_headline_view.js
@@ -16,8 +16,11 @@ module.exports = View.extend({
   
   afterRender: function(){
     this.$('th').droppable({
+        accept: "#headlines li",
         drop: function(event,ui){
-          event.target.innerHTML = event.srcElement.innerHTML;
+          event.target.innerHTML = ui.draggable.text();
+          ui.draggable.hide(200);
+          //event.target.innerHTML = event.srcElement.innerHTML;
         }
       });
   }
