bb088945f6920c68004240e4b9e808fe022295d4
Jan Monschke <jan.monschke@gmail.com>
2012-05-23 23:42:11 +0200

    call super method

--- app/models/collection.js
+++ app/models/collection.js
@@ -1,4 +1,7 @@
 // Base class for all collections.
 module.exports = Backbone.Collection.extend({
-  
+  initialize: function(){
+    // call super method
+    Backbone.Model.prototype.initialize.call(this);
+  }
 });
