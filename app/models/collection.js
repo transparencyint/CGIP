8a291ec94d190a603d739b1e2d44f36f50220bd6
Jan Monschke <jan.monschke@gmail.com>
2012-05-25 13:45:13 +0200

    add destroyAll method to the base collection

--- app/models/collection.js
+++ app/models/collection.js
@@ -3,5 +3,26 @@ module.exports = Backbone.Collection.extend({
   initialize: function(){
     // call super method
     Backbone.Model.prototype.initialize.call(this);
+  },
+
+  destroyAll: function(options){
+    if(!options) options = {};
+    if(!options.success) options.success = function(){};
+    if(!options.error) options.error = function(){};
+
+    var docs = {
+      docs: this.toJSON()
+    };
+
+    var db = Backbone.couch_connector.helpers.make_db();
+
+    db.bulkRemove(docs, {
+      success: function(){
+        options.success();
+      },
+      error: function(errNr, errMsg){
+        options.error(errNr, errMsg);
+      }
+    });
   }
 });
