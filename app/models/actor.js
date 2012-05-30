b32f17893654c74c8694a65932bea94fdc9cd3a2
Jan Monschke <jan.monschke@gmail.com>
2012-05-30 14:33:02 +0200

    enable selecting actors and dragging actor groups

--- app/models/actor.js
+++ app/models/actor.js
@@ -3,6 +3,13 @@ module.exports = Backbone.Model.extend({
 
   defaults : {
     name : ''
+  },
+
+  moveByDelta: function(delta){
+    var thisPos = _.clone(this.get('pos'));
+    thisPos.x += delta.x;
+    thisPos.y += delta.y;
+    this.set('pos', thisPos);
   }
 
 });
