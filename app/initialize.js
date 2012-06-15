ca2212ae9bee14850aca431f03e1835b45b049f7
Jan Monschke <jan.monschke@gmail.com>
2012-06-15 02:39:40 +0200

    disable proxy-urls for now

--- app/initialize.js
+++ app/initialize.js
@@ -11,7 +11,7 @@ $(function() {
 });
 
 // overwrite the defaul requests, because /db is the new db namespace
-$.ajaxPrefilter(function( options, originalOptions, jqXHR ) {
-  if(window.location.href.indexOf('localhost:3000') != -1 || window.location.href.indexOf('127.0.0.1:3000') != -1)
-    options.url = '/db' + options.url;
-});
+// $.ajaxPrefilter(function( options, originalOptions, jqXHR ) {
+//   if(window.location.href.indexOf('localhost:3000') != -1 || window.location.href.indexOf('127.0.0.1:3000') != -1)
+//     options.url = '/db' + options.url;
+// });
