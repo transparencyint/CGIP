23d790ec3568f6c39fda2a6146b35f330ad39983
Jan Monschke <jan.monschke@gmail.com>
2012-06-26 17:51:07 +0200

    remove unneeded lines

--- app/initialize.js
+++ app/initialize.js
@@ -10,9 +10,3 @@ $(function() {
   application.initialize();
   Backbone.history.start();
 });
-
-// overwrite the defaul requests, because /db is the new db namespace
-// $.ajaxPrefilter(function( options, originalOptions, jqXHR ) {
-//   if(window.location.href.indexOf('localhost:3000') != -1 || window.location.href.indexOf('127.0.0.1:3000') != -1)
-//     options.url = '/db' + options.url;
-// });
