1dec450742488fbfbc445df5dc0d4758d327f778
Jan Monschke <jan.monschke@gmail.com>
2012-07-10 19:14:47 +0200

    first version of money connection edit list
    features:
    
    - show all connections with their attributes
    - support for null-connections
    - save connection on change of actors

---
+++ app/views/connections_list_entry.js
@@ -0,0 +1,49 @@
+var View = require('./view');
+
+module.exports = View.extend({
+
+  template: require('./templates/connections_list_entry'),
+  tagName: 'tr',
+
+  initialize: function(){
+    _.bindAll(this, 'actorChanged');
+  },
+
+  actorChanged: function(){
+    console.log('change');
+    var newFrom = this.$('.from-actors-select').val();
+    var newTo = this.$('.to-actors-select').val();
+
+    if(newFrom !== newTo){
+      // none means no connection
+      if(newTo == 'none') newTo = null;
+      if(newFrom == 'none') newFrom = null;
+
+      this.model.save({from: newFrom, to: newTo});
+    }else
+      alert('A connection hast to have two different actors.');
+  },
+
+  getRenderData: function(){
+    var data = this.model.toJSON()
+    data.actors = this.options.actors.toJSON();
+    return data;
+  },
+
+  afterRender: function(){
+    // select the current actors
+    var from = this.model.get('from');
+    var to = this.model.get('to');
+    if(from)
+      this.$('.from-actors-select option[value=' + from + ']').attr('selected', 'selected');
+    else
+      this.$('.from-actors-select option').last().attr('selected', 'selected');
+    if(to)
+      this.$('.to-actors-select option[value=' + to + ']').attr('selected', 'selected');
+    else
+      this.$('.to-actors-select option').last().attr('selected', 'selected');
+
+    // bind to select changes
+    this.$('.from-actors-select, .to-actors-select').change(this.actorChanged);
+  }
+});
---
+++ app/views/connections_list_view.js
@@ -0,0 +1,32 @@
+var View = require('./view');
+var ConnectionListEntry = require('./connections_list_entry');
+
+// Base class for all views.
+module.exports = View.extend({
+
+  template: require('./templates/connections_list'),
+  tagName: 'table',
+
+  initialize: function(){
+    this.collection.on('add', this.addOne, this);
+    this.collection.on('reset', this.render, this);
+  },
+
+  addOne: function(connection){
+    var newView = new ConnectionListEntry({model: connection, actors: this.options.actors});
+    this.$el.append(newView.render().el);
+  },
+
+  render: function(){
+    var list = this;
+
+    this.$el.append(this.template());
+
+    this.collection.each(function(connection){
+      list.addOne(connection);
+    });
+
+    return this;
+  }
+
+});
