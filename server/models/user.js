f7ad6cd24ca783f6afc1c20e6360569dbd73886c
Jan Monschke <jan.monschke@gmail.com>
2012-07-12 00:12:15 +0200

    don't cache anything

--- server/models/user.js
+++ server/models/user.js
@@ -1,25 +1,26 @@
 var config = require('../config').config;
 var crypto = require('crypto');
 var connection = require('../db/database_connection').connection.createConnection();
+var db = connection.database('cgip_users');
 
 exports.User = {
   get: function(id, done){
-    connection.database('cgip_users').get(String(id), function(err, doc){
+    db.get(String(id), function(err, doc){
       done(err, doc);
     });
   },
 
   findByName: function(name, done){
-    connection.database('cgip_users').get(name, function(err, doc){
+    db.get(name, function(err, doc){
       done(err, doc);
     });
   },
 
   create: function(username, password, done){
     var pwhash = this.getSaltedHash(username, password);
-    connection.database('cgip_users').get(username, function(err, doc){
+    db.get(username, function(err, doc){
       if(err && err.error == 'not_found')
-        connection.database('cgip_users').save(username, {
+        db.save(username, {
           id: username,
           pwhash: pwhash
         }, function(err, res){
