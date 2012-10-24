277d73ded01d50594c59e55b025e4b11e710eef6
Jan Monschke <jan.monschke@gmail.com>
2012-10-24 15:03:40 +0200

    fix some stuff in the navigation view, add locationType and add comments

--- app/views/navigation_view.js
+++ app/views/navigation_view.js
@@ -7,14 +7,18 @@ module.exports = View.extend({
   
   className: 'navigation controls top left',
   
-  _currentLocation: '',
-  _countryTable: {
+  currentRoute: '',
+  currentLocation: '',
+  countryTable: {
     'pe': 'Peru',
     'do': 'Dom. Republic',
     'mx': 'Mexico',
     'bd': 'Bangladesh',
     'ke': 'Kenya',
     'md': 'Maldives'
+  },
+  locationTable: {
+    'money_connections_list': 'Money List'
   },
 
   initialize: function(options){
@@ -23,14 +27,28 @@ module.exports = View.extend({
   
   getRenderData: function(){
     return { 
-      locationName: this._countryTable[ this._currentLocation ] 
+      locationName: this.countryTable[ this.currentLocation ],
+      locationType: this.locationTable[ this.currentRoute ]
     };
   },
   
-  update: function(location, country){
-    if(this._currentLocation) this.$el.removeClass( this._currentLocation );
-    this._currentLocation = country;
-    this.$el.addClass( this._currentLocation );
+  update: function(action, country){
+    // parse the route from the action
+    var route = action.split('route:')[1];
+
+    // return if it wasn't a 'route:' event
+    if(!route) return;
+
+    // remove the current location
+    if(this.currentLocation) this.$el.removeClass( this.currentLocation );    
+
+    if(country){
+      this.currentLocation = country;
+      this.$el.addClass( this.currentLocation );  
+    }
+    
+    this.currentRoute = route;
+
     this.render();
   }
   
