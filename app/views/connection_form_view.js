4e9147e7890cffe99c08572fcd8b87cf5107fb38
Iyad Al <i.alsahwi@hotmail.de>
2012-11-04 22:03:51 +0100

    allow only numbers in money amount input

--- app/views/connection_form_view.js
+++ app/views/connection_form_view.js
@@ -27,12 +27,14 @@ module.exports = View.extend({
 
     var amount = this.model.get('amount');
     if(typeof(amount) !== 'undefined')
-      this.$el.find(".amount").val(amount);
+      this.$('.amount').val(amount);
     else
-      this.$el.find(".amount").val('100000');
+      this.$('.amount').val('100000');
 
     var elem = this.$el;
     var model = this.model;    
+
+    this.$('.amount')..numeric();
 
     this.$('.amount').draggableInput({
       type: 'integer',
