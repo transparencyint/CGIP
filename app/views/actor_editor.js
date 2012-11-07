0a5b7508461b1c24e249ba607a5edc926145d6fe
Jan Monschke <jan.monschke@gmail.com>
2012-11-07 14:48:42 +0100

    deactivate modes, fixes #34

--- app/views/actor_editor.js
+++ app/views/actor_editor.js
@@ -162,7 +162,7 @@ module.exports = View.extend({
   },
 
   deactivateMode: function(){
-    this.$('.connections li').removeClass('active');
+    this.$('.connection').removeClass('active');
     this.mode.abort();
     this.mode = null;
 
