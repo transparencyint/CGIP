3f357a2432dd96199a7918a4d0bf0fbad30adea3
Jan Monschke <jan.monschke@gmail.com>
2012-11-21 00:08:26 +0100

    add url function

--- app/models/country.js
+++ app/models/country.js
@@ -1,5 +1,11 @@
 var Model = require('./model');
 
 module.exports = Model.extend({
-  
+  url: function(){
+    if(this.id){
+      return '/countries/' + this.id;
+    }else{
+      return '/countries'
+    }
+  }
 });
