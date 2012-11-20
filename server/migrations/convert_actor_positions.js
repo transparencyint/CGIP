86b13fb1013c2f0933f79a910f49e00a1cf085b6
Jan Monschke <jan.monschke@gmail.com>
2012-11-20 17:33:37 +0100

    work on more sophisticated migration

--- server/migrations/convert_actor_positions.js
+++ server/migrations/convert_actor_positions.js
@@ -1,5 +1,6 @@
 var Actor = require('../models/actor').Actor;
 var _ = require('underscore');
+var migrationID = 'convert_actor_positions';
 
 console.log('Running position migration on all countries...');
 
@@ -17,7 +18,19 @@ Actor.allByCountry('pe', function(err, actors){
       console.log('Errors:', errors);
     }
   };
+  console.log('Calculating bounding box...');
+  var minX = _.min(actors, function(actor){ return actor.pos.x; }).pos.x;
+  var maxX = _.max(actors, function(actor){ return actor.pos.x; }).pos.x;
+  var subsctract = minX + ((maxX - minX) / 2);
+  console.log('minX:', minX, 'maxX:', maxX);
+  
   _.each(actors, function(actor){
-    Actor.edit(actor._id, actor, countDown);
-  })
+    if(!actor.migrations) actor.migrations = {};
+    // only execute this migration if not already processed
+    if(!actor.migrations[migrationID]){
+      actor.migrations.push(migrationID);
+      actor.pos.x -= subsctract;
+      Actor.edit(actor._id, actor, countDown);
+    }
+  });
 })
