84e73dbc9c40228e8481a59cbb3fc0fb160d0f84
Jan Monschke <jan.monschke@gmail.com>
2012-12-03 14:06:46 +0100

    add draggable view and inherit actor view from it

--- app/views/actor_view.js
+++ app/views/actor_view.js
@@ -1,37 +1,34 @@
-var View = require('./view');
+var DraggableView = require('./draggable_view');
 var LightboxView = require('./lightbox_view');
 
-module.exports = View.extend({
+module.exports = DraggableView.extend({
   
   template : require('./templates/actor'),
   
   className : 'actor',
 
-  events: {
-    'dblclick .name': 'startEditName',
-    'dblclick .abbrev': 'startEditAbbrev',
-    'blur .abbrev-input': 'stopEditAbbrev',
-    'blur .name-input': 'stopEditName',
-    'keydown input': 'saveOnEnter',
-    'mousedown .inner': 'select',
-    'dblclick' : 'showMetadataForm',
-    'mousedown': 'dragStart'
+  events: function(){
+    var parentEvents = DraggableView.prototype.events;
+    // merge the parent events and the current events
+    return _.defaults({
+      'dblclick .name': 'startEditName',
+      'dblclick .abbrev': 'startEditAbbrev',
+      'blur .abbrev-input': 'stopEditAbbrev',
+      'blur .name-input': 'stopEditName',
+      'keydown input': 'saveOnEnter',
+      'mousedown .inner': 'select',
+      'dblclick' : 'showMetadataForm'
+    }, parentEvents);
   },
   
   initialize: function(options){
-    _.bindAll(this, 'dragStop', 'drag');
+    DraggableView.prototype.initialize.call(this, options);
 
-    this.editor = options.editor;
     this.width = this.editor.radius*2;
-    this.height = this.editor.radius*2;
-    this.dontDrag = false;
-
-    this.editor.on('disableDraggable', this.disableDraggable, this);
-    this.editor.on('enableDraggable', this.enableDraggable, this);
+    this.height = this.editor.radius*2;    
 
     this.model.on('change:abbreviation', this.updateAbbrev, this);
     this.model.on('change:name', this.updateName, this);
-    this.model.on('change:pos', this.updatePosition, this);
     this.model.on('change:role', this.drawRoleBorders, this);
     this.model.on('destroy', this.modelDestroyed, this);
   },
@@ -39,14 +36,6 @@ module.exports = View.extend({
   showMetadataForm: function(){
     this.lightboxView = new LightboxView({model : this.model});
     $(document.body).append(this.lightboxView.render().el);
-  },
-
-  disableDraggable: function(){
-    this.dontDrag = true;
-  },
-
-  enableDraggable: function(){
-    this.dontDrag = false;
   },
 
   select: function(event){
@@ -154,78 +143,6 @@ module.exports = View.extend({
 
   modelDestroyed: function(){
     this.$el.remove();
-  },
-  
-  dragStart: function(event){
-    if(!this.dontDrag){
-      event.stopPropagation();
-      
-      var pos = this.model.get('pos');
-      
-      this.startX = event.pageX - pos.x;
-      this.startY = event.pageY - pos.y;
-    
-      $(document).on('mousemove.global', this.drag);
-      $(document).one('mouseup', this.dragStop);
-    }
-  },
-
-  drag: function(event){ 
-    var pos = this.model.get('pos');
-    
-    var dx = (event.pageX - pos.x - this.startX) / this.editor.zoom.value;
-    var dy = (event.pageY - pos.y - this.startY) / this.editor.zoom.value;
-    
-    this.editor.dragGroup(dx, dy);
-  },
-  
-  updatePosition: function(){
-    var pos = this.model.get('pos');
-    
-    this.$el.css({
-      left: pos.x,
-      top: pos.y
-    });
-  },
-  
-  dragStop : function(){
-    this.snapToGrid();    
-    $(document).unbind('mousemove.global');
-  },
-  
-  snapToGrid: function(){
-    //make drag available along a simple grid
-    var gridSize = this.editor.gridSize;
-    var pos =  this.model.get('pos');     
-
-    //move the actor to the nearest grid point
-    var x = Math.round(pos.x / gridSize) * gridSize;
-    var y = Math.round(pos.y / gridSize) * gridSize;
-    
-    var dx = x - pos.x;
-    var dy = y - pos.y;
-    
-    if(dx !== 0 || dy !== 0){
-      var editor = this.editor;
-
-      $({percent: 0}).animate({percent: 1}, {
-        step: function(){
-          var stepX = this.percent * dx;
-          var stepY = this.percent * dy;
-
-          editor.dragGroup(stepX, stepY);
-
-          dx -= stepX;
-          dy -= stepY;
-        },
-        duration: 100,
-        complete: function(){
-          editor.saveGroup();
-        }
-      });
-    } else {
-      this.editor.saveGroup();
-    }
   },
   
   getRenderData : function(){
---
+++ app/views/draggable_view.js
@@ -0,0 +1,100 @@
+var View = require('./view');
+
+module.exports = View.extend({
+  events: {
+      'mousedown': 'dragStart'
+  },
+
+  initialize: function(){
+    View.prototype.initialize.call(this);
+    _.bindAll(this, 'dragStop', 'drag');
+
+    this.editor = this.options.editor;
+    this.dontDrag = false;
+
+    this.editor.on('disableDraggable', this.disableDraggable, this);
+    this.editor.on('enableDraggable', this.enableDraggable, this);
+
+    this.model.on('change:pos', this.updatePosition, this);
+  },
+
+  disableDraggable: function(){
+    this.dontDrag = true;
+  },
+
+  enableDraggable: function(){
+    this.dontDrag = false;
+  },
+
+  dragStart: function(event){
+    if(!this.dontDrag){
+      event.stopPropagation();
+      
+      var pos = this.model.get('pos');
+      
+      this.startX = event.pageX - pos.x;
+      this.startY = event.pageY - pos.y;
+    
+      $(document).on('mousemove.global', this.drag);
+      $(document).one('mouseup', this.dragStop);
+    }
+  },
+
+  drag: function(event){ 
+    var pos = this.model.get('pos');
+    
+    var dx = (event.pageX - pos.x - this.startX) / this.editor.zoom.value;
+    var dy = (event.pageY - pos.y - this.startY) / this.editor.zoom.value;
+    
+    this.editor.dragGroup(dx, dy);
+  },
+  
+  updatePosition: function(){
+    var pos = this.model.get('pos');
+    
+    this.$el.css({
+      left: pos.x,
+      top: pos.y
+    });
+  },
+  
+  dragStop : function(){
+    this.snapToGrid();    
+    $(document).unbind('mousemove.global');
+  },
+
+  snapToGrid: function(){
+    //make drag available along a simple grid
+    var gridSize = this.editor.gridSize;
+    var pos =  this.model.get('pos');     
+
+    //move the actor to the nearest grid point
+    var x = Math.round(pos.x / gridSize) * gridSize;
+    var y = Math.round(pos.y / gridSize) * gridSize;
+    
+    var dx = x - pos.x;
+    var dy = y - pos.y;
+    
+    if(dx !== 0 || dy !== 0){
+      var editor = this.editor;
+
+      $({percent: 0}).animate({percent: 1}, {
+        step: function(){
+          var stepX = this.percent * dx;
+          var stepY = this.percent * dy;
+
+          editor.dragGroup(stepX, stepY);
+
+          dx -= stepX;
+          dy -= stepY;
+        },
+        duration: 100,
+        complete: function(){
+          editor.saveGroup();
+        }
+      });
+    } else {
+      this.editor.saveGroup();
+    }
+  }
+});
