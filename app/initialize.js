447c657c32e5b8eac7eeb228c0c9dbe8d517dad5
Jan Monschke <jan.monschke@gmail.com>
2012-12-10 13:21:38 +0100

    add csrf protection - fixes #76

--- app/initialize.js
+++ app/initialize.js
@@ -11,3 +11,11 @@ $(function() {
     Backbone.history.start({pushState: true});
   });
 });
+
+// inject the csrf token on every request
+var csrf_token = null;
+$.ajaxPrefilter(function(options, originalOptions, jqXHR) {
+  if(!options.headers) options.headers = {};
+  if(!csrf_token) csrf_token = $('meta[name=csrf_token]').attr('content');
+  options.headers['x-csrf-token'] = csrf_token;
+});
