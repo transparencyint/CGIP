4ae09f2efc118b1dee2895dbd52686f9783f626d
Iyad Al <i.alsahwi@hotmail.de>
2012-12-17 19:16:55 +0100

    set default model

--- app/models/country.js
+++ app/models/country.js
@@ -2,10 +2,15 @@ var Model = require('./model');
 
 module.exports = Model.extend({
   url: function(){
-    if(this.id){
-      return '/countries/' + this.id;
-    }else{
-      return '/countries'
-    }
-  }
+		if(this.id){
+		  return '/countries/' + this.id;
+		} else{
+		  return '/countries'
+		}
+  },
+
+	defaults : {
+		roleDimensions: [ -500, -250, 0, 250, 500 ]
+	}
+
 });
