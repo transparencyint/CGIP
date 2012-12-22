55b531a3d7b9f9be0f4401a543e8e4a5153755bb
Jan Monschke <jan.monschke@gmail.com>
2012-12-22 22:31:13 +0100

    some refactorings

--- app/models/connections/money_connection.js
+++ app/models/connections/money_connection.js
@@ -13,7 +13,6 @@ module.exports = Connection.extend({
   calculateCoinSize: function(){
     // Don't execute when the model hasn't been added to a collection yet
     if(!this.collection) return
-
     debugger
     var amountType = config.get('moneyConnectionMode').replace('Mode','');
     var amount = this.get(amountType);
@@ -28,18 +27,19 @@ module.exports = Connection.extend({
     
     //there is at least 1 other money connection on the map already
     if(size > 1){
-      maxMoneyAmount = this.collection.min(function(connection){return connection.get(amountType)}).get(amountType);
-      minMoneyAmount = this.collection.max(function(connection){return connection.get(amountType)}).get(amountType);
+      var amountTypeSelect = function(connection){ return connection.get(amountType); };
+      maxMoneyAmount = this.collection.min(amountTypeSelect).get(amountType);
+      minMoneyAmount = this.collection.max(amountTypeSelect).get(amountType);
 
       console.log("minMoneyAmount"+minMoneyAmount);
       console.log("maxMoneyAmount"+maxMoneyAmount);
 
       var isMinMaxEqual = minMoneyAmount === maxMoneyAmount;
-      var minCoinFactor = this.minCoinSizeFactor;
 
       //connections have at least 1 different money value
       //moneyRange can't be 0, because in a later calculation divide by 0 is not possible
       if(!isMinMaxEqual){
+        var minCoinFactor = this.minCoinSizeFactor;
         var factorRange = this.maxCoinSizeFactor - minCoinFactor; 
         var moneyRange = maxMoneyAmount - minMoneyAmount;
 
