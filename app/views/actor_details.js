0124527a50a86876c5cb130d6809a97054726f13
quetschman86 <info@tommykrueger.com>
2013-02-03 11:22:00 +0100

    made textarea autosize cal simpler

--- app/views/actor_details.js
+++ app/views/actor_details.js
@@ -246,7 +246,6 @@ module.exports = View.extend({
     this.clickCatcher = new clickCatcher({ callback: this.submitAndClose, holder: this.editor.$el });
     
     $(document).keydown(this.handleKeys);
-    this.autosize = this.$('textarea').autosize({ className: 'actorDetailsAutosizeHelper' });
     this.holder = this.$('.holder');
     
     // focus first input field
@@ -260,8 +259,8 @@ module.exports = View.extend({
       
       self.$('input').first().focus();
 
-      // fixes wron height calculation of the textareas
-      self.$('textarea').trigger('autosize');
+      // fixes wrong height calculation of the textareas
+      self.autosize = self.$('textarea').autosize({ className: 'actorDetailsAutosizeHelper' });
     });
   },
   
