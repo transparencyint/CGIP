cd5837978370ea871ade16125f26d6362e4ce38e
Iyad Al <i.alsahwi@hotmail.de>
2013-03-04 20:10:33 +0100

    missing check for other actor type

--- app/views/actor_view.js
+++ app/views/actor_view.js
@@ -85,7 +85,8 @@ module.exports = DraggableDroppableView.extend({
     return { 
       name: this.determineName(), 
       orgaType: this.orgaType,
-      organizationType: this.model.get('organizationType')
+      organizationType: this.model.get('organizationType'),
+      typeOther: this.model.get('typeOther')
     };
   },
 
